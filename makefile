44a3bad2857464984690cf49d69daa1f1d8bed08
Sho Iwamoto <contact@misho-web.com>
2018-06-15 12:25:38 +0200

    chore: append tests in makefile

--- makefile
+++ makefile
@@ -4,3 +4,23 @@
 clean:
 	python setup.py clean
 	rm -rf simsusy.egg-info htmlcov .coverage simsusy/__pycache__ simsusy/tests/__pycache__
+
+.PHONY: test unittest typetest formattest
+test: unittest typetest formattest
+
+unittest:
+	@echo "# Performing the tests..."
+	nosetests --config="" --cover-package=simsusy --with-coverage
+	@echo
+
+typetest:
+	@echo "# Checking typing by mypy..."
+	mypy --ignore-missing-imports --follow-imports=silent .
+	@echo
+
+formattest:
+	@echo "# Checking PEP format..."
+	flake8 --max-line-length=120 .
+	@echo
+
+
--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ import ast
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('simsusy/simsusy.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1)))
+    version_match = _version_re.search(f.read().decode('utf-8'))
+    version = str(ast.literal_eval(version_match.group(1))) if version_match else '?.?.?'
 
 setup(
     name='simsusy',
@@ -17,7 +18,7 @@ setup(
     entry_points={
         'console_scripts': 'generate_spectrum = simsusy.simsusy.generate_spectrum'
     },
-    tests_require=['nose', 'coverage'],
+    tests_require=['nose', 'coverage', 'mypy', 'flake8'],
     test_suite='nose.collector',
     classifiers=[
         'Environment :: Console',
