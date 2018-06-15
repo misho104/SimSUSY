0fb9f21a012860475045d647ab4641db8251c660
Sho Iwamoto <contact@misho-web.com>
2018-06-15 23:02:41 +0200

    feat: add utility functions (matrix decomposition algorithms)

--- simsusy/tests/test_utility.py
+++ simsusy/tests/test_utility.py
@@ -1,13 +1,52 @@
-from nose.tools import ok_  # noqa
-import simsusy.utility as u
 import logging
-import unittest
-import random
 import math
+import random
+import unittest
+import numpy as np
+from nose.tools import ok_, eq_  # noqa
+import simsusy.utility as u
+from typing import Optional  # noqa
+
 logger = logging.getLogger('test_info')
 
 
 class TestUtility(unittest.TestCase):
+    def __assert_sub(self, test: bool, msg: str, a: np.ndarray, b: Optional[np.ndarray]=None):
+        if test:
+            ok_(True)
+        else:
+            logger.info(f'Tested objects are\n{a}\n{str(b)}\n')
+            self.fail(msg)
+
+    def assertMatrixAlmostEqual(self, a: np.ndarray, b: np.ndarray):
+        self.__assert_sub(np.allclose(a, b), 'Matrices not identical', a, b)
+
+    def assertMatrixEqual(self, a: np.ndarray, b: np.ndarray):
+        self.__assert_sub((a == b).all(), 'Matrices not identical', a, b)
+
+    def assertMatrixAllReal(self, a: np.ndarray):
+        self.__assert_sub(np.isreal(a).all(), 'Matrix not real', a)
+
+    def assertMatrixUnitary(self, a: np.ndarray):
+        self.__assert_sub(a.ndim == 2 and a.shape[0] == a.shape[1]
+                          and np.allclose(a @ np.conjugate(a.T), np.identity(a.shape[0])),
+                          'Matrix not unitary', a)
+
+    def assertSortedInAbsoluteValue(self, a: np.ndarray):
+        target = a.flatten()
+        for i in range(0, len(target) - 1):
+            self.__assert_sub(abs(target[i]) < abs(target[i + 1]), 'Object not sorted in its absolute value', a)
+
+    def assertSorted(self, a: np.ndarray):
+        target = a.flatten()
+        for i in range(0, len(target) - 1):
+            self.__assert_sub(target[i] < target[i + 1], 'Object not sorted', a)
+
+    def assertAllNonNegative(self, a: np.ndarray):
+        target = a.flatten()
+        for i in range(0, len(target)):
+            self.__assert_sub(target[i] >= 0, 'Object not all-positive', a)
+
     def test_trigonometric_random(self):
         for i in range(0, 4):
             theta = random.uniform(i * math.pi / 2, (i + 1) * math.pi / 2)
@@ -37,3 +76,104 @@ class TestUtility(unittest.TestCase):
         self.assertAlmostEqual(0, u.cos2tan(-1))
         self.assertAlmostEqual(0, u.tan2sintwo(0))
         self.assertAlmostEqual(1, u.tan2costwo(0))
+        self.assertAlmostEqual(1, u.tan2cos(0))
+        self.assertAlmostEqual(0, u.sin2tan(0))
+        self.assertAlmostEqual(0, u.cos2tan(1))
+        self.assertAlmostEqual(0, u.cos2tan(-1))
+        self.assertAlmostEqual(0, u.tan2sintwo(0))
+        self.assertAlmostEqual(1, u.tan2costwo(0))
+
+    def test_chop_matrix(self):
+        r = 1e-12
+        i = 1e-12j
+        # remove off-diagonal small elements
+        self.assertMatrixEqual(u.chop_matrix(np.array([[1, r, i], [i, 2, r], [r, i, -4j + i]])),
+                               np.array([[1, 0, 0], [0, 2, 0], [0, 0, -4j + i]]))
+        # not remove if one of the relevant diagonal elements are small
+        self.assertMatrixEqual(u.chop_matrix(np.array([[1, r, i], [i, 2, r], [i, r, r]])),
+                               np.array([[1, 0, i], [0, 2, r], [i, r, r]]))
+        # remove small real or imaginary part
+        self.assertMatrixEqual(u.chop_matrix(
+            np.array([[3 + i, r + 3j, 3 + 3j, r + 2 * i], [3 + r + i, r + 3j + i, - r + i, i]])),
+            np.array([[3, 3j, 3 + 3j, r + 2 * i], [3 + r, 3j + i, - r + i, i]]))
+        # combination
+        self.assertMatrixEqual(u.chop_matrix(np.array([[1 + i, r, i], [r + i, 2 + i, r], [-r, i, -4j + i + r]])),
+                               np.array([[1, 0, 0], [0, 2, 0], [0, 0, -4j + i]]))
+
+    def test_autonne_takagi_real(self):
+        m = np.random.rand(4, 4) - np.random.rand(4, 4)  # generate signed random matrix
+        m = m + m.T  # input must be symmetric
+
+        # for real input, Autonne-Takagi decomposition will give
+        #   - real eigenvalues with real mixing matrix (for try_real_mixing = True)
+        #   - positive eigenvalues with complex mixing matrix (for try_real_mixing = False)
+
+        d_vector, n = u.autonne_takagi(m, try_real_mixing=True)
+        self.assertMatrixAllReal(n)
+        self.assertSortedInAbsoluteValue(d_vector)
+        self.assertMatrixUnitary(n)
+        self.assertMatrixAlmostEqual(np.conjugate(n) @ m @ np.conjugate(n.T), np.diag(d_vector))
+
+        d_vector, n = u.autonne_takagi(m, try_real_mixing=False)
+        self.assertAllNonNegative(d_vector)
+        self.assertSortedInAbsoluteValue(d_vector)
+        self.assertMatrixUnitary(n)
+        self.assertMatrixAlmostEqual(np.conjugate(n) @ m @ np.conjugate(n.T), np.diag(d_vector))
+
+    def test_autonne_takagi_complex(self):
+        m = (np.random.rand(4, 4) - np.random.rand(4, 4)) + (np.random.rand(4, 4) - np.random.rand(4, 4)) * 1j
+        m = m + m.T  # input must be symmetric
+
+        # for complex input, Autonne-Takagi decomposition will give
+        #   - real eigenvalues with complex mixing matrix (for try_real_mixing = True)
+        #   - positive eigenvalues with complex mixing matrix (for try_real_mixing = False)
+
+        d_vector, n = u.autonne_takagi(m, try_real_mixing=True)
+        self.assertSortedInAbsoluteValue(d_vector)
+        self.assertMatrixUnitary(n)
+        self.assertMatrixAlmostEqual(np.conjugate(n) @ m @ np.conjugate(n.T), np.diag(d_vector))
+
+        d_vector, n = u.autonne_takagi(m, try_real_mixing=False)
+        self.assertAllNonNegative(d_vector)
+        self.assertSortedInAbsoluteValue(d_vector)
+        self.assertMatrixUnitary(n)
+        self.assertMatrixAlmostEqual(np.conjugate(n) @ m @ np.conjugate(n.T), np.diag(d_vector))
+
+    def test_singular_value_decomposition_real(self):
+        m = (np.random.rand(4, 4) - np.random.rand(4, 4))
+        d_vector, uu, vv = u.singular_value_decomposition(m)
+        self.assertAllNonNegative(d_vector)
+        self.assertSorted(d_vector)
+        self.assertMatrixUnitary(uu)
+        self.assertMatrixUnitary(vv)
+        self.assertMatrixAllReal(uu)
+        self.assertMatrixAllReal(vv)
+        self.assertMatrixAlmostEqual(np.conjugate(uu) @ m @ np.conjugate(vv.T), np.diag(d_vector))
+
+    def test_singular_value_decomposition_complex(self):
+        m = (np.random.rand(4, 4) - np.random.rand(4, 4)) + (np.random.rand(4, 4) - np.random.rand(4, 4)) * 1j
+        d_vector, uu, vv = u.singular_value_decomposition(m)
+        self.assertAllNonNegative(d_vector)
+        self.assertSorted(d_vector)
+        self.assertMatrixUnitary(uu)
+        self.assertMatrixUnitary(vv)
+        self.assertMatrixAlmostEqual(np.conjugate(uu) @ m @ np.conjugate(vv.T), np.diag(d_vector))
+
+    def test_mass_diagonalization_real(self):
+        m = (np.random.rand(4, 4) - np.random.rand(4, 4))
+        m = m + m.T  # symmetric
+        d_vector, r = u.mass_diagonalization(m)
+        self.assertMatrixAllReal(d_vector)
+        self.assertSorted(d_vector)
+        self.assertMatrixUnitary(r)
+        self.assertMatrixAllReal(r)
+        self.assertMatrixAlmostEqual(r @ m @ np.conjugate(r.T), np.diag(d_vector))
+
+    def test_mass_diagonalization_complex(self):
+        m = (np.random.rand(4, 4) - np.random.rand(4, 4)) + (np.random.rand(4, 4) - np.random.rand(4, 4)) * 1j
+        m = m + np.conjugate(m.T)  # hermitian
+        d_vector, r = u.mass_diagonalization(m)
+        self.assertMatrixAllReal(d_vector)
+        self.assertSorted(d_vector)
+        self.assertMatrixUnitary(r)
+        self.assertMatrixAlmostEqual(r @ m @ np.conjugate(r.T), np.diag(d_vector))
--- simsusy/utility.py
+++ simsusy/utility.py
@@ -1,3 +1,7 @@
+from typing import Tuple
+import numpy as np
+import numpy.linalg as LA
+import cmath
 
 
 def sin2cos(sin: float)->float:
@@ -37,3 +41,70 @@ def tan2costwo(tan: float)->float:
 def tan2sintwo(tan: float)->float:
     """returns Abs[Sin[2*ArcTan[x]]]"""
     return abs(2 * tan / (tan**2 + 1))
+
+
+def chop_matrix(m: np.ndarray, threshold=1E-7):
+    nx, ny = m.shape
+    for ix in range(0, nx):
+        for iy in range(0, ny):
+            v = m[ix, iy]
+            # chop element if smaller than "key entries"
+            if ix != iy and abs(v) < min(abs(m[ix, min(ix, ny - 1)]), abs(m[min(iy, nx - 1), iy])) * threshold:
+                m[ix, iy] = 0
+            # chop imaginary part if small
+            elif v.real != 0 and v.imag != 0:
+                ratio = abs(v.imag / v.real)
+                if ratio < threshold:
+                    m[ix, iy] = v.real
+                elif ratio > 1 / threshold:
+                    m[ix, iy] = v.imag * 1j
+    return m
+
+
+def autonne_takagi(m: np.ndarray, try_real_mixing=True)->Tuple[np.ndarray, np.ndarray]:
+    """Perform Autonne-Takagi decomposition.
+
+    :param m: an input matrix M.
+    :param try_real_mixing: if true, try to set N as real by allowing negative D entries;
+                            if false, D is positive and N may be complex.
+    :returns: a tuple (d, N), where d is a 1d matrix containing the diagonal elements of
+              a diagonal matrix D, and N is an unitary matrix, which satisfy N^* M N^† = D. (SLHA eq.12)
+              N is real if possible and try_real_mixing=True, and d is sorted as ascending in its absolute value.
+    """
+    eigenvalues, eigenvectors = LA.eigh(np.conjugate(m) @ m)
+    n = np.conjugate(eigenvectors.T)
+    if try_real_mixing:
+        phases = np.diag([abs(x) / x for x in n.diagonal()])
+    else:
+        d = (np.conjugate(n) @ m @ np.conjugate(n.T)).diagonal()
+        phases = np.diag([cmath.exp(-cmath.phase(x) / 2j) for x in d])
+    n = phases @ n
+    return chop_matrix((np.conjugate(n) @ m @ np.conjugate(n.T))).diagonal(), n
+
+
+def singular_value_decomposition(m: np.ndarray)->Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Perform singular value decomposition.
+
+    :param m: an input matrix M.
+    :returns: a tuple (d, U, V), where d is a 1d matrix containing the diagonal elements of
+              a non-negative diagonal matrix D, and U and V are unitary matrices, which satisfy
+              U^* M V^† = D. (SLHA eq.14 or SLHA2 eq.48)
+              U and V are real for a real input M, and d is ascending.
+    """
+    u0, s, vh0 = LA.svd(m)  # u0 @ s @ vh0 = m, i.e. u0^† @ m @ v0h^† = s
+    d, u, v = s[::-1], (u0.T)[::-1], vh0[::-1]  # to have ascending order
+    return d, chop_matrix(u), chop_matrix(v)
+
+
+def mass_diagonalization(m: np.ndarray)->Tuple[np.ndarray, np.ndarray]:
+    """Perform mass diagonalization.
+
+    :param m: an input matrix M, which is Hermitian.
+    :returns: a tuple (d, R), where d is a 1d matrix containing the diagonal elements of
+              a real diagonal matrix D, and R is an unitary matrix, which satisfy
+              R M R^† = D. (SLHA eq.16 and SLHA2 below Eq.10)
+              R is real for a real input M, and d is ascending.
+    """
+    eigenvalues, eigenvectors = LA.eigh(m)
+    r = np.conjugate(eigenvectors.T)
+    return eigenvalues, chop_matrix(r)
