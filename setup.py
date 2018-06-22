02bbbed4dde109bd66f4ea98f4f29ba7a1b8a788
Sho Iwamoto <contact@misho-web.com>
2018-06-22 16:16:37 +0200

    feat: prepare setup script

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 #!env python
 from setuptools import setup
-import re
 import ast
+import pathlib
+import re
 
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('simsusy/simsusy.py', 'rb') as f:
+root = pathlib.Path('simsusy')
+with (root / 'simsusy.py').open('rb')as f:
     version_match = _version_re.search(f.read().decode('utf-8'))
     version = str(ast.literal_eval(version_match.group(1))) if version_match else '?.?.?'
+
+packages = ['simsusy']
+for subdir in root.iterdir():
+    if subdir.is_dir() and subdir.name[0:4] != 'test' and re.match(r'[a-zA-Z]', subdir.name[0]):
+        packages.append(str(subdir).replace('/', '.'))
 
 setup(
     name='simsusy',
     version=version,
-    packages=['simsusy'],
+    packages=packages,
     install_requires=['click', 'pyslha', 'numpy'],
     entry_points={
-        'console_scripts': 'generate_spectrum = simsusy.simsusy.generate_spectrum'
+        'console_scripts': 'simsusy = simsusy.simsusy:simsusy_main'
     },
     tests_require=['nose', 'coverage', 'mypy', 'flake8'],
     test_suite='nose.collector',
