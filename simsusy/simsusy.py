b19d18ed7db3036c2b9a9569980265c9865cdbb8
Sho Iwamoto <contact@misho-web.com>
2018-07-01 15:23:02 +0200

    feat: prepare runner

--- simsusy/simsusy.py
+++ simsusy/simsusy.py
@@ -6,7 +6,7 @@ import inspect
 import pathlib
 import types
 from logging import basicConfig, getLogger, DEBUG
-from typing import Dict  # noqa: F401
+from typing import KeysView, ValuesView, Dict, Optional  # noqa: F401
 
 import simsusy
 
@@ -19,11 +19,49 @@ basicConfig(level=DEBUG)
 logger = getLogger(__name__)
 cwd = pathlib.Path(__file__).parent.resolve()
 
-calculators = dict()   # type: Dict[str, pathlib.Path]
-for calculator_file in cwd.glob('*/*_calculator.py'):
-    relative_path = calculator_file.resolve().relative_to(cwd)
-    module_name = '.'.join(relative_path.with_suffix('').parts)
-    calculators[module_name] = pathlib.Path('simsusy') / relative_path
+
+class Calculators:
+    """A singleton class to store calculators."""
+
+    def __init__(self):
+        cwd = pathlib.Path(__file__).parent.resolve()
+        self.calculators = dict()   # type: Dict[str, pathlib.Path]
+        for calculator_file in cwd.glob('*/*_calculator.py'):
+            relative_path = calculator_file.resolve().relative_to(cwd)
+            module_name = '.'.join(relative_path.with_suffix('').parts)
+            self.calculators[module_name] = pathlib.Path('simsusy') / relative_path
+
+    def __getitem__(self, *args, **kwargs):
+        return self.calculators.__getitem__(*args, **kwargs)
+
+    def get(self, name: str) -> Optional[pathlib.Path]:
+        return self.calculators.get(name)
+
+    def guess(self, name: str)->Optional[pathlib.Path]:
+        if name in self.calculators:
+            return self.calculators[name]
+        if name.count('.') != 1:
+            return None
+        model, calc = name.lower().split('.')
+        candidates = list()
+        for i in self.calculators.keys():
+            i_model, i_calc = i.lower().split('.')
+            if i_model.startswith(model) and i_calc.startswith(calc):
+                candidates.append(i)
+        if len(candidates) == 1:
+            return self.calculators[candidates[0]]
+        else:
+            if candidates:
+                logger.error(f'The calculator specification {name} is ambiguous:')
+                for i in candidates:
+                    logger.error(f'\t{i}')
+            return None
+
+    def keys(self) -> KeysView[str]:
+        return self.calculators.keys()
+
+    def values(self)->ValuesView[pathlib.Path]:
+        return self.calculators.values()
 
 
 @click.group(help='Handle the references for high-energy physics',
@@ -44,16 +82,19 @@ def simsusy_main(context, **kwargs):
 @click.argument('output', type=click.Path(dir_okay=False), required=False)
 @click.pass_context
 def run(context, calculator, input, output):
-    if calculator not in calculators:
+    calculators = Calculators()
+    guessed_calculator = calculators.guess(calculator)
+    if guessed_calculator is None:
         logger.error(f'calculator "{calculator}" not found.\n\nAvailable calculators are:')
         max_length = max(len(name) for name in calculators.keys())
-        for name, path in calculators.items():
-            logger.error(f'\t{name:<{max_length}}\t({path})')
+        for name in calculators.keys():
+            logger.error(f'\t{name:<{max_length}}\t({calculators[name]})')
         exit(1)
+    module_name = '.'.join(guessed_calculator.with_suffix('').parts)
     try:
-        mod = importlib.import_module(f'simsusy.{calculator}')
+        mod = importlib.import_module(module_name)
     except ModuleNotFoundError as e:
-        logger.error(f'Calculator {calculator} cannot be imported.\n')
+        logger.error(f'Calculator {calculator} cannot be imported.')
         if context.obj['DEBUG']:
             raise e
         else:
@@ -66,7 +107,7 @@ def run(context, calculator, input, output):
            issubclass(mod.Input, simsusy.abs_model.AbsModel) and
            issubclass(mod.Model, simsusy.abs_model.AbsModel)
            ):
-        logger.error(f'Calculator {calculator} imported but invalid.\n')
+        logger.error(f'Calculator {calculator} imported but invalid.')
         if context.obj['DEBUG']:
             logger.error('Debug information:')
             logger.error(f'\tCalculator\t{mod.Calculator}')
