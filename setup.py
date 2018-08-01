027aebbba65515b1b01126da3ba04f24b30ff5df
Sho Iwamoto <contact@misho-web.com>
2018-08-01 18:05:02 +0200

    debug: correct setup.py

--- setup.py
+++ setup.py
@@ -5,22 +5,32 @@ import pathlib
 import re
 
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-root = pathlib.Path('simsusy')
-with (root / 'simsusy.py').open('rb')as f:
-    version_match = _version_re.search(f.read().decode('utf-8'))
-    version = str(ast.literal_eval(version_match.group(1))) if version_match else '?.?.?'
+with (pathlib.Path('simsusy') / 'simsusy.py').open('rb') as f:
+    version_match = re.search(r'__version__\s+=\s+(.*)', f.read().decode('utf-8'))
+    version = str(ast.literal_eval(version_match.group(1))) if version_match else '0.0.0'
 
 packages = ['simsusy']
-for subdir in root.iterdir():
-    if subdir.is_dir() and subdir.name[0:4] != 'test' and re.match(r'[a-zA-Z]', subdir.name[0]):
+for subdir in pathlib.Path('simsusy').iterdir():
+    if subdir.is_dir() \
+        and (not subdir.name.startswith('test')) \
+        and re.match(r'[a-zA-Z]', subdir.name[0]):
         packages.append(str(subdir).replace('/', '.'))
 
 setup(
     name='simsusy',
     version=version,
+    author='Sho Iwamoto / Misho',
+    author_email='webmaster@misho-web.com',
+    url='https://github.com/misho104/simsusy',
+    description='TBW...',
+    python_requires='>=3.4',
+    license='MIT',
     packages=packages,
+    zip_safe=False,   # for default_values.json
+    package_data={
+        'simsusy': [
+            'default_values.json',
+        ]},
     install_requires=['click', 'yaslha', 'numpy'],
     dependency_links=[
       'git+https://github.com/misho104/yaslha.git#egg=yaslha'
