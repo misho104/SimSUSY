8473659be6fcddcde925894b207617137d9d0b8a
Sho Iwamoto <contact@misho-web.com>
2022-06-11 15:37:22 +0200

    0.2.0

--- simsusy/simsusy.py
+++ simsusy/simsusy.py
@@ -13,7 +13,7 @@ import simsusy.abs_calculator
 import simsusy.abs_model
 
 __pkgname__ = "SimSUSY"
-__version__ = "0.0.1"
+__version__ = "0.2.0"
 __author__ = "Sho Iwamoto / Misho"
 __license__ = "MIT"
 
